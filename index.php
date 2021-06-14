9c0b9c876783049b29f9e42058c1a56645c2b479
Kis Dennis <5114@htl.rennweg.at>
2021-06-14 12:48:59 +0200

    Header

--- index.php
+++ index.php
@@ -4,19 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
     <title>Logger Blogger</title>
 </head>
 <body>
 
-    <header>
+    <div class="header">
+        <h1>Logger Blogger</h1>
+    </div> 
 
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Beiträge</a></li>
-            <li><a href="#">Login</a></li>
+    <div>
+        <ul class="nav-content">
+            <li><a class="nav-item" href="#">Home</a></li>
+            <li><a class="nav-item" href="#">Beiträge</a></li>
+            <li><a class="nav-item" href="login-form.html">Login</a></li>
         </ul>
-        
-    </header>
+    </div>
     
 </body>
 </html>
