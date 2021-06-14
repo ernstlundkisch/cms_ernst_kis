ccec16083090bd44ba8fa679b76c1cbc5210e939
Kis Dennis <5114@htl.rennweg.at>
2021-06-14 13:41:55 +0200

    CSS und Flexbox für Nav

--- index.php
+++ index.php
@@ -4,16 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.typekit.net/knl8yeg.css">
     <link href="style.css" rel="stylesheet">
     <title>Logger Blogger</title>
 </head>
 <body>
 
     <div class="header">
-        <h1>Logger Blogger</h1>
+        <h1>Logger</h1>
+        <h1>Blogger</h1>
     </div> 
 
-    <div>
+    <div class="nav">
         <ul class="nav-content">
             <li><a class="nav-item" href="#">Home</a></li>
             <li><a class="nav-item" href="#">Beiträge</a></li>
